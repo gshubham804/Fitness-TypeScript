5e7fbf4837d7f9f5932811e4b37ebabd78ef3ec3
SHUBHAM GUPTA <gshubham804@gmail.com>
2023-05-05 18:09:02 +0530

    aws issues fixed

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import Amplify from 'aws-amplify';
-import awsconfig from './aws-exports.js';
+import {Amplify} from 'aws-amplify';
+import awsconfig from '@/aws-exports';
 
 Amplify.configure(awsconfig);
 
